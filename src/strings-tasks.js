63d7b481d391398bf2d68cc756779a68228e5d58
Nikita Pankov <genozu@yandex.by>
2023-11-16 17:32:20 +0300

    fix: add correction

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -238,8 +238,8 @@ function endsWith(str, substr) {
  *   formatTime(0, 45) => "00:45"
  *   formatTime(0, 0) => "00:00"
  */
-function formatTime(minutes, seconds) {
-  return minutes * seconds;
+function formatTime(/* minutes, seconds */) {
+  throw new Error('Not implemented');
 }
 
 /**
@@ -267,8 +267,8 @@ function reverseString(str) {
  *   orderAlphabetically('textbook') => 'bekoottx'
  *   orderAlphabetically('abc123xyz') => '123abcxyz'
  */
-function orderAlphabetically(str) {
-  return str.split('').sort().join('');
+function orderAlphabetically(/* str */) {
+  throw new Error('Not implemented');
 }
 
 /**
@@ -283,8 +283,8 @@ function orderAlphabetically(str) {
  *   containsSubstring('JavaScript is Fun', 'Python') => false
  *   containsSubstring('12345', '34') => true
  */
-function containsSubstring(str, substring) {
-  return str.includes(substring);
+function containsSubstring(/* str, substring */) {
+  throw new Error('Not implemented');
 }
 
 /**
